65bb8d9a9df45be0649d23f8946f02b3dc641358
00iun <lsh681236@naver.com>
2025-02-24 10:23:19 +0900

    '1일차'

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// console.log("Hello TypeScript")
+
+// const nums: number[] = []
+
+// const a = 10 + nums[0]
+
+// const someString = 'typescript!';
+// someString.endsWith('!')
+
+import { printName } from "./util";
+
+printName('typescirpt')
